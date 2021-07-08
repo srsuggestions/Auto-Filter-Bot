5256e37ac6afd3f7a4892e0fad5040af430441e0
srsuggestions <87122362+srsuggestions@users.noreply.github.com>
2021-07-08 14:58:38 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -159,7 +159,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("HELP", callback_data="help_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/Mo_Tech_YT")]
+                [InlineKeyboardButton("⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/srsuggestionsmc")]
             ])
 
             await query.message.edit_text(
@@ -174,7 +174,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="start_data"),
                     InlineKeyboardButton("ABOUT", callback_data="about_data")],
-                [InlineKeyboardButton("⭕️ SUPPORT ⭕️", url="https://youtube.com/channel/UCmGBpXoM-OEm-FacOccVKgQ")]
+                [InlineKeyboardButton("⭕️ CHANNEL ⭕️", url="https://t.me/srsuggestionsmc")]
             ])
 
             await query.message.edit_text(
