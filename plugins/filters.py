950eb29b98275d20b397b150eb649b6e7bc68b00
srsuggestions <87122362+srsuggestions@users.noreply.github.com>
2021-07-08 15:12:11 +0530

    Update filters.py

--- plugins/filters.py
+++ plugins/filters.py
@@ -189,7 +189,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             keyboard = InlineKeyboardMarkup([
                 [InlineKeyboardButton("BACK", callback_data="help_data"),
                     InlineKeyboardButton("START", callback_data="start_data")],
-                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/MRK-YT/Auto-Filter-Bot")]
+                [InlineKeyboardButton("SOURCE CODE", url="https://github.com/srsuggestions/Auto-Filter-Bot")]
             ])
 
             await query.message.edit_text(
